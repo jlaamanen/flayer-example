55205325d82635e22bd880fb904bf13a5bc990a0
Joona Laamanen <joona.laamanen@gmail.com>
2022-10-15 00:59:55 +0300

    Updated Flayer version

--- server-pkg/index.d.ts
+++ server-pkg/index.d.ts
@@ -1,5 +1,5 @@
 /// <reference path="./products/index.d.ts" />
 /// <reference path="./user/index.d.ts" />
 declare module "server-pkg" {
-    export { configure, disconnect } from "flayer/client-lib";
+    export { configure, disconnect } from "flayer/dist/client-lib";
 }
